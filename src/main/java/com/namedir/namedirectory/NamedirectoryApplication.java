b316bbe9d99381085558af907c2033b9f908c712
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2025-11-25 16:52:01 +0800

    chore: initial commit

---
+++ src/main/java/com/namedir/namedirectory/NamedirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.namedir.namedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamedirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamedirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namedir/namedirectory/NamedirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namedir.namedirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
