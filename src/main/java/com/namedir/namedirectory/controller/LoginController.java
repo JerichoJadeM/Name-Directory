f2af5ecd29143b5f4a758789d9ae9ae2c5a26416
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2025-12-03 19:56:41 +0800

    feat(auth): implement user registration to enable login and access

--- src/main/java/com/namedir/namedirectory/controller/LoginController.java
+++ src/main/java/com/namedir/namedirectory/controller/LoginController.java
@@ -1,15 +1,44 @@
 package com.namedir.namedirectory.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.namedir.namedirectory.dto.UserRegistrationDto;
+import com.namedir.namedirectory.service.UserRegistrationService;
 
 @Controller
 public class LoginController {
+
+    private final UserRegistrationService userRegistrationService;
+
+    public LoginController(UserRegistrationService userRegistrationService) {
+        this.userRegistrationService = userRegistrationService;
+    }
     
     @GetMapping("/login")
     public String showLoginPage(){
 
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+
+        model.addAttribute("user", new UserRegistrationDto());
+
+        return "registration-form";
+    }
+
+    @PostMapping("/register/save")
+    public String registerUser(@ModelAttribute("user") UserRegistrationDto dto){
+
+        userRegistrationService.registerUser(dto);
+
+        return "redirect:/login?success";
+      
     }
 
 }
--- src/main/java/com/namedir/namedirectory/security/SecurityConfig.java
+++ src/main/java/com/namedir/namedirectory/security/SecurityConfig.java
@@ -5,17 +5,42 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+    @Bean
+    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+
+        // read users
+        manager.setUsersByUsernameQuery(
+            "select username, password, enabled from app_users where username=?");
+        manager.setAuthoritiesByUsernameQuery(
+            "Select username, authority from authorities where username=?");
+
+
+        //write users
+        // manager.setCreateUserSql(
+        //     "INSERT INTO app_users (username, password, enabled) VALUES (?,?,?)"
+        // );
+
+        // manager.setCreateAuthoritySql(
+        //     "INSERT INTO authorities (username, authority) VALUES (?,?)"
+        // );
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -23,6 +48,7 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(configurer -> configurer
                     .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                    .requestMatchers("/login", "/authenticateUser", "/register", "/register/**").permitAll()
                     .anyRequest().authenticated())
 
                     .formLogin(form -> form.loginPage("/login")
@@ -40,8 +66,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
-
 
 }
